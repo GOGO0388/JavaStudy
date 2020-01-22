a1ede519c18db2d4473569e2ff80de442a1d62a3
GOGO0388 <asdfg0388@naver.com>
2020-01-22 12:37:56 +0900

    04. 연산자 실습 p.79

---
+++ JavaStudy/src/study04/Comp.java
@@ -0,0 +1,19 @@
+package study04;
+
+public class Comp {
+
+	public static void main(String[] args) {
+		
+		double e=3.1;
+		e+=2.1;
+		e*=2;
+		
+		int n=5;
+		n*=2.2;
+		
+		System.out.println(e);
+		System.out.println(n);
+
+	}
+
+}
