b78b84185cdaf8b43f333277471036f69a5efa1a
GOGO0388 <asdfg0388@naver.com>
2020-01-22 14:20:57 +0900

    04. 연산자 실습 문제4-1

---
+++ JavaStudy/src/study04/Practice.java
@@ -0,0 +1,17 @@
+package study04;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		int num1=10, num2=20, num3=30;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+
+		num3=num2=num1;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+	}
+
+}
---
+++ JavaStudy/src/study04/Practice2.java
@@ -0,0 +1,15 @@
+package study04;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A = ((25+5)+(36/4)-72)*5;
+		int B = ((25*5)+(36-4)+71)/4;
+		int C = (128/4)*2;
+		
+		boolean isBig = (A>B) && (B>C);
+		System.out.println(isBig);
+	}
+
+}
