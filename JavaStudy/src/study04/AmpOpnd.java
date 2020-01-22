5b0604d13b8e3402c5797c7d772f4609250749c8
GOGO0388 <asdfg0388@naver.com>
2020-01-22 12:20:51 +0900

    04. 연산자 실습

---
+++ JavaStudy/src/study04/AmpOpnd.java
@@ -0,0 +1,11 @@
+package study04;
+
+public class AmpOpnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(7.2%2.0);
+		System.out.println(7.2%2.1);
+	}
+
+}
